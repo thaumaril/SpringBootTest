089c938c97f4e5ad64df1a75a1bc27fbb285bbf8
Oliver Winkelmann <OliverWinkelmann@hotmail.com>
2019-02-20 18:48:24 +0100

    Update

--- hello-world/src/main/java/com/example/helloworld/Greeting.java
+++ hello-world/src/main/java/com/example/helloworld/Greeting.java
@@ -1,10 +1,5 @@
 package com.example.helloworld;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 public class Greeting{
 
     private final Long id;
--- hello-world/src/test/java/com/example/helloworld/DemoApplicationTests.java
+++ hello-world/src/test/java/com/example/helloworld/DemoApplicationTests.java
@@ -5,9 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
